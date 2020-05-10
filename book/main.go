e2103b79ec54f355f14d422fbd0406d185200910
DSobkov <danylo.sobkov@ukr.net>
2020-05-10 17:23:18 +0300

    1

---
+++ book/main.go
@@ -0,0 +1,94 @@
+package main
+
+import (
+	"errors"
+	"github.com/dgrijalva/jwt-go"
+	"net/http"
+	"time"
+
+	"github.com/gorilla/mux"
+	"golang.org/x/crypto/bcrypt"
+	"log"
+)
+
+//jwt token
+var mySigningKey = []byte("secret")
+var GetTokenHandler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request){
+	/* Create the token */
+	token := jwt.New(jwt.SigningMethodHS256)
+
+	/* Create a map to store our claims */
+	claims := token.Claims.(jwt.MapClaims)
+
+	/* Set token claims */
+	claims["admin"] = true
+	claims["name"] = "dafdgdsds"
+	claims["exp"] = time.Now().Add(time.Hour * 24).Unix()
+
+	/* Sign the token with our secret */
+	tokenString, _ := token.SignedString(mySigningKey)
+
+	/* Finally, write the token to the browser window */
+	w.Write([]byte(tokenString))
+})
+
+
+
+
+
+
+
+//password
+type User struct {
+	Id       int64  `json:"id"`
+	Username string `json:"username"`
+	Hash     string `json:"-"`
+	Password string `json:"password"`
+}
+
+func Get(r *http.Request, key interface{}) interface{} {
+	return r.Context().Value(key)
+}
+
+var ErrInvalidPassword = errors.New("Invalid Password")
+var ErrPasswordMismatch = errors.New("Password cannot be blank")
+var ErrEmptyPassword = errors.New("No password provided")
+
+func ChangePassword(r *http.Request) error {
+	u := Get(r, "user").(User)
+	currentPw := r.FormValue("current_password")
+	newPassword := r.FormValue("new_password")
+	confirmPassword := r.FormValue("confirm_new_password")
+	// Check the current password
+	err := bcrypt.CompareHashAndPassword([]byte(u.Hash), []byte(currentPw))
+	if err != nil {
+		return ErrInvalidPassword
+	}
+	// Check that the new password isn't blank
+	if newPassword == "" {
+		return ErrEmptyPassword
+	}
+	// Check that new passwords match
+	if newPassword != confirmPassword {
+		return ErrPasswordMismatch
+	}
+	// Generate the new hash
+	h, err := bcrypt.GenerateFromPassword([]byte(newPassword), bcrypt.DefaultCost)
+	if err != nil {
+		return err
+	}
+	u.Hash = string(h)
+	return nil
+}
+
+
+func main() {
+	//Init Router
+	r := mux.NewRouter()
+
+	r.Handle("/get-token", GetTokenHandler).Methods("GET")
+
+
+
+	log.Fatal(http.ListenAndServe(":8001", r))
+}
---
+++ test.go
@@ -0,0 +1,164 @@
+package Course
+
+import (
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	"github.com/gorilla/mux"
+	_ "github.com/lib/pq"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+
+
+//Book Struct (Model)
+type Book struct {
+	ID    string `json:"ID"`
+	Title string `json:"Title"`
+}
+
+//Init database and error vars
+var db *sql.DB
+var err error
+var books []Book
+//Get All Books
+func getBooks(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	var books []Book
+
+	result, err := db.Query("SELECT * from Books")
+	if err != nil {
+		panic(err.Error())
+	}
+
+	defer result.Close()
+
+	for result.Next() {
+
+		var book Book
+
+		err := result.Scan(&book.ID, &book.Title)
+		if err != nil {
+			panic(err.Error())
+		}
+
+		books = append(books, book)
+	}
+
+	json.NewEncoder(w).Encode(books)
+}
+
+//Get Single Book
+func getBook(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r) //Get params
+
+	result, err := db.Query("SELECT * FROM Books WHERE ID = @ID", sql.Named("ID", params["ID"]))
+	if err != nil {
+		panic(err.Error())
+	}
+
+	defer result.Close()
+
+	var book Book
+
+	for result.Next() {
+		err := result.Scan(&book.ID, &book.Title)
+		if err != nil {
+			panic(err.Error())
+		}
+	}
+
+	json.NewEncoder(w).Encode(book)
+}
+
+//Create a New Book
+func createBook(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	keyVal := make(map[string]string)
+	json.Unmarshal(body, &keyVal)
+	ID := keyVal["ID"]
+	newTitle := keyVal["Title"]
+
+	result, err := db.Query("INSERT INTO Books(ID,Title) VALUES(@ID,@newTitle)", sql.Named("ID", ID), sql.Named("newTitle", newTitle))
+	if err != nil {
+		panic(err.Error())
+	}
+
+	defer result.Close()
+
+	fmt.Fprintf(w, "New book was created")
+
+}
+
+//Update Book
+func updateBook(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	keyVal := make(map[string]string)
+	json.Unmarshal(body, &keyVal)
+	newTitle := keyVal["Title"]
+
+	result, err := db.Query("UPDATE Books SET Title = @newTitle WHERE ID = @ID", sql.Named("newTitle", newTitle), sql.Named("ID", params["ID"]))
+	if err != nil {
+		panic(err.Error())
+	}
+
+	defer result.Close()
+
+	fmt.Fprintf(w, "Book with ID = %s was updated", params["ID"])
+
+}
+
+//Delete Book
+func deleteBook(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+
+	result, err := db.Query("DELETE FROM Books WHERE ID = @ID", sql.Named("ID", params["ID"]))
+	if err != nil {
+		panic(err.Error())
+	}
+
+	defer result.Close()
+
+	fmt.Fprintf(w, "Book with ID = %s was deleted", params["ID"])
+
+}
+
+func main() {
+	db, err = sql.Open("postgres" , "postgresql://localhost:5432/ims")
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	defer db.Close()
+
+	//Init Router
+	r := mux.NewRouter()
+
+	//Route Handlers/Endpoints
+	r.HandleFunc("/books", getBooks).Methods("GET")
+	r.HandleFunc("/books/{ID}", getBook).Methods("GET")
+	r.HandleFunc("/books", createBook).Methods("POST")
+	r.HandleFunc("/books/{ID}", updateBook).Methods("PUT")
+	r.HandleFunc("/books/{ID}", deleteBook).Methods("DELETE")
+
+	log.Fatal(http.ListenAndServe(":8001", r))
+}
